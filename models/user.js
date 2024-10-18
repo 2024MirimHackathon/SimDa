7367a8db982b9a3c79e02b13dd2f6db1533772d5
dayul <dyulchoo@naver.com>
2024-10-18 22:48:19 +0900

    [fix] DB syntax error 해결

--- models/user.js
+++ models/user.js
@@ -1,25 +1,30 @@
-const {DataTypes} = require("sequelize");
+const { DataTypes } = require("sequelize");
 
-const User = sequelize => sequelize.define("User", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true, // 기본 키로 설정
-    autoIncrement: true,
-    allowNull: false
+const User = (sequelize) => sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true, 
+      autoIncrement: true,
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
   },
-  userId: {
-    type: DataTypes.STRING(100),
-    allowNull: false
-  },
-  name: {
-    type: DataTypes.STRING(100),
-    allowNull: false
-  },
-  password: {
-    type: DataTypes.STRING(100),
-    allowNull: false
-  },
-  timestamps: false,
-});
+  {
+    timestamps: false, 
+  }
+);
 
 module.exports = User;
