8c3a7629f3aa6cfe10ea506cbe53de2743bac59a
김가현 <rkgus0324@gmail.com>
2024-10-19 00:46:25 +0900

    index.ejs, style.css

--- public/js/cardToImage.js
+++ public/js/cardToImage.js
@@ -2,18 +2,27 @@ async function captureCard() {
     const card = document.getElementById('card');
 
     // html2canvas를 사용하여 카드 캡쳐
-    html2canvas(card).then(canvas => {
+    html2canvas(card).then(async (canvas) => {
         // 캔버스를 이미지 데이터 URL로 변환
         const dataURL = canvas.toDataURL('image/png');
 
-        // 다운로드 링크 생성
-        const link = document.createElement('a');
-        link.href = dataURL;
-        link.download = 'captured_card.png'; // 다운로드할 파일 이름
+        // 사용자 ID를 가져옵니다. (예시로 getUserId() 함수 사용)
+        const userId = await getUserId(); // 사용자 ID 요청
 
-        // 링크 클릭으로 다운로드
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link); // 링크 요소 제거
+        try {
+            // 서버에 이미지 데이터 전송
+            const response = await fetch(`/image_paths/${userId}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ imageUrl: dataURL })
+            });
+
+            const result = await response.text();
+            console.log("결과:", result);
+        } catch (error) {
+            console.error('Error:', error);
+        }
     });
 }
